f336cbb44cb62e7168a8366fb3ede343894ea7a1
OmarOsorio11 <43758080+OmarOsorio11@users.noreply.github.com>
2023-01-28 00:48:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function showone() {
+    document.getElementById("mOne").style.visibility = "visible";
+}
+
+function closeone() {
+    document.getElementById("mOne").style.visibility = "hidden";
+}
+function showtwo() {
+    document.getElementById("mTwo").style.visibility = "visible";
+}
+
+function closetwo() {
+    document.getElementById("mTwo").style.visibility = "hidden";
+}
